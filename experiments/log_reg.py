30f38887798d5248f494c73b1a4f04cfee7a4d84
Greg <ggundersen@gmail.com>
2017-04-18 15:38:46 -0400

    Latest paper version.

--- experiments/log_reg.py
+++ experiments/log_reg.py
@@ -4,14 +4,20 @@
 import data
 from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+
+
+plt.style.use('seaborn')
 
 
 def main():
-    X_train, y_train, X_test, y_test = data.load('subsample', n=1000000, pct_pos=0.75)
+    X_train, y_train, X_test, y_test = data.load('subsample', n=1000, pct_pos=0.75)
     print('Data subsampled.')
     clf   = LogisticRegression()
     clf.fit(X_train, y_train)
-    preds = clf.predict(X_test)
+    preds = clf.predict_proba(X_test)
+    preds = preds[:, 1]  # Select the predictions for positive class.
+    save_roc_curve_data(y_test, preds)
     acc   = metrics.accuracy_score(y_test, preds)
     prec  = metrics.precision_score(y_test, preds)
     rec   = metrics.recall_score(y_test, preds)
@@ -21,6 +27,12 @@ def main():
     with open('experiments/log_reg_results.csv', 'w+') as f:
         line = '%s,%s,%s,%s,%s' % (acc, prec, rec, f1, auc)
         f.write(line)
+
+
+def save_roc_curve_data(y_test, preds):
+    fpr, tpr, thresh = metrics.roc_curve(y_test, preds)
+    plt.plot(fpr, tpr)
+    plt.show()
 
 
 if __name__ == '__main__':
--- experiments/online.py
+++ experiments/online.py
@@ -7,23 +7,20 @@ from sklearn.linear_model import SGDClassifier
 import numpy as np
 
 
-def run_experiments(n_batches):
-
+def run_experiments(n_batches, pct_pos):
     # Online algorithms in sklearn:
     # http://scikit-learn.org/stable/modules/scaling_strategies.html
     clf = SGDClassifier()
-    with open('sgd_out.txt', 'w+') as f:
+    with open('sgd_out_%s.txt' % pct_pos, 'w+') as f:
         for i in range(n_batches):
-            acc, prec, rec, f1, auc = run_experiment(clf)
+            acc, prec, rec, f1, auc = run_experiment(clf, pct_pos)
             print(acc, prec, rec, f1, auc)
             line = '%s,%s,%s,%s,%s' % (acc, prec, rec, f1, auc)
             f.write(line)
-            if i == n_batches-1:
-                print('FINAL -------')
-                print(acc, prec, rec, f1, auc)
 
-def run_experiment(clf):
-    X_train, y_train, X_test, y_test = data.load('subsample', n=100000, pct_pos=0.01)
+
+def run_experiment(clf, pct_pos):
+    X_train, y_train, X_test, y_test = data.load('subsample', n=100000, pct_pos=pct_pos)
     X_train = np.delete(X_train, [0, 1], axis=1)
     X_test = np.delete(X_test, [0, 1], axis=1)
     print('Data subsampled.')
@@ -34,12 +31,10 @@ def run_experiment(clf):
     rec   = metrics.recall_score(y_test, preds)
     f1    = metrics.f1_score(y_test, preds)
     auc   = metrics.roc_auc_score(y_test, preds)
-    # with open('experiments/logisticreg_results.csv', 'w+') as f:
-    #     line = '%s,%s,%s,%s,%s' % (acc, prec, rec, f1, auc)
-    #     f.write(line)
     return acc, prec, rec, f1, auc
 
 
 if __name__ == '__main__':
     N_BATCHES = 100
-    run_experiments(N_BATCHES)
+    for pct_pos in [0.75, 0.5, 0.1, 0.01]:
+        run_experiments(N_BATCHES, pct_pos)
